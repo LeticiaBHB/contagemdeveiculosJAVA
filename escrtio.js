2df5bb483459f3d59c1aa41ed042b8d0895a9564
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-06-07 01:46:29 -0300

    first commit

---
+++ escrtio.js
@@ -0,0 +1,7 @@
+function toggleText() {
+	var textElement = document.getElementById("text");
+	var contentElement = document.getElementById("content");
+
+	textElement.style.display = "none";
+	contentElement.style.display = "block";
+}
